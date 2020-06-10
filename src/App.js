17b5c2258101da2b396f7a4adf9873584e52f5d4
Michael Cassaro <mdcassaro@hotmail.com>
2020-06-10 13:49:10 -0500

    updated routing to match project name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App(){
         <Navbar/>
         <Wrapper>
           <Route exact path="/" component={About} />
-          <Route exact path="/react-portfolio-final" component={About}/>
+          <Route exact path="/my-portfolio" component={About}/>
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
