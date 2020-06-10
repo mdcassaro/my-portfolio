272e40d0094ec5e8da6045ade957c39e20a20f99
Michael Cassaro <mdcassaro@hotmail.com>
2020-06-10 13:25:26 -0500

    contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/Contact.css";
+
+
+
+
+function Contact(){
+    return(
+        <div class="container align-self-center">
+            <h1>My Contact Information:</h1>
+        
+            <div className="row justify-content-around">
+
+                <a href="https://www.linkedin.com/in/michael-cassaro-5580161a6/" target="_blank" rel= "noopener noreferrer"class="btn btn-primary">Visit my Linkedin</a>
+            
+
+           
+
+            <a href="mailto:mdcassaro@hotmailmail.com" target="_blank" rel= "noopener noreferrer"  class="btn btn-primary">Click to Email Me</a>
+                   
+                
+            </div>
+                    
+                
+
+           
+      </div>
+      )
+    }
+
+    export default Contact;
